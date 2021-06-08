45d2dbddbcce824e9684a7c01cc45e556f80f9a9
Simon Elbrink <simon.elbrink@gmail.com>
2021-06-08 11:02:24 +0200

    Answer question regarding Array Assignment How to use a the methods.

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,7 +1,5 @@
 package se.lexicon;
 
-import java.util.Arrays;
-
 /**
  * Hello world!
  *
@@ -10,6 +8,28 @@ public class App
 {
     public static void main( String[] args )
     {
+        //How i t could look like using the method for the assignment.
+        //Have not implemented them of a purpose.
 
+        String name = "Simon Elbrin";
+        boolean wasAdded = add(name);
+
+        update("Simon Elbrin", "Simon Elbrink");
+
+        System.out.println("wasAdded = " + wasAdded);
+
+        //CRUD - Create, Read, Update, Delete
+
+    }
+
+    public static boolean update(final String original, final String updatedName){
+        return true;
+    }
+
+
+    public static boolean add(final String fullName){
+
+
+        return true;
     }
 }
