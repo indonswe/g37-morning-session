2e4051879b0bf6872f14cdd64c647edb2653c80e
Simon Elbrink <simon.elbrink@gmail.com>
2021-06-08 10:27:06 +0200

    Extracting index-finding behavior to a method.

--- src/main/java/se/lexicon/array/exercise/Exercise2.java
+++ src/main/java/se/lexicon/array/exercise/Exercise2.java
@@ -10,16 +10,23 @@ public class Exercise2 {
 
         int indexToFind = 43;
 
-        Arrays.sort(numbers);
-        int index = Arrays.binarySearch(numbers, indexToFind);
+        int returnValue = indexOf(numbers, indexToFind);
+
+        System.out.println("returnValue = " + returnValue);
+
+    }
+
+    private static int indexOf(int[] source, int indexToFind) {
+        Arrays.sort(source);
+        int index = Arrays.binarySearch(source, indexToFind);
 
         if(index >= 0) {
             System.out.println("Index Found!");
             System.out.println("Element: " + indexToFind +" was located at index: " + index);
-
+            return index;
         }else {
             System.out.println("Could not find element: " + indexToFind);
+            return -1;
         }
-
     }
 }
