3f1d004fd7cd1932a984fe2ce6c526604cd08629
indonswe <afc2002afc@hotmail.com>
2021-06-08 19:55:30 +0200

    My initial commit

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -6,11 +6,13 @@ package se.lexicon;
  */
 public class App 
 {
+    static String[] names  = new String[0];
     public static void main( String[] args )
     {
         //How i t could look like using the method for the assignment.
         //Have not implemented them of a purpose.
 
+        //String names [] = new String[5];
         String name = "Simon Elbrin";
         boolean wasAdded = add(name);
 
@@ -18,17 +20,58 @@ public class App
 
         System.out.println("wasAdded = " + wasAdded);
 
+        String name1 = "Adrian Nilsso";
+        wasAdded = add(name1);
+
+        update("Adrian Nilsso", "Adrian Nilsson");
+
+        System.out.println("wasAdded = " + wasAdded);
+
+
         //CRUD - Create, Read, Update, Delete
 
     }
 
     public static boolean update(final String original, final String updatedName){
-        return true;
+
+        int nl = names.length;
+        //System.out.println("update: " + nl);
+    for (int i = 0;i < names.length; i++)
+    {
+        //System.out.println("i: " + i);
+        //System.out.println("names: " + names[i]);
+        if (names[i].equals(original)) {
+            System.out.println(names[i]);
+            names[i] = updatedName;
+            System.out.println(names[i]);
+        }
+
     }
 
-
+        return true;
+    }
     public static boolean add(final String fullName){
+        int nl = names.length+1;
+        String [] newArray = new String[nl];
 
+        newArray[nl-1] = fullName;
+        for (int i = 0;i < names.length; i++)
+        {
+            //System.out.println("i: " + i);
+            //System.out.println("names: " + names[i]);
+            newArray[i] = names[i];
+
+        }
+        newArray[nl-1] = fullName;
+        names = newArray;
+        //System.out.println(nl);
+        //System.out.println(newArray[nl-1]);
+
+        /*for (int i = 0;i < names.length; i++)
+        {
+            names[i]=fullName;
+            newArray
+        }*/
 
         return true;
     }
