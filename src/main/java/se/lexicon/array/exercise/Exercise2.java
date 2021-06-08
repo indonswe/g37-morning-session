c6ffca9917c905ea862eff02bbd72564ce9ad6d2
Simon Elbrink <simon.elbrink@gmail.com>
2021-06-08 10:29:35 +0200

    Extracting number Array to a Field. to give a hint on the assignment.

--- src/main/java/se/lexicon/array/exercise/Exercise2.java
+++ src/main/java/se/lexicon/array/exercise/Exercise2.java
@@ -4,21 +4,22 @@ import java.util.Arrays;
 
 public class Exercise2 {
 
+    //Data Storage | DataBase | List of numbers | Array Of Numbers
+    private static int[] numbers = {43,346,237,2348,567,345};
+
     public static void main( String[] args )
     {
-        int[] numbers = {43,346,237,2348,567,345};
-
         int indexToFind = 43;
 
-        int returnValue = indexOf(numbers, indexToFind);
+        int returnValue = indexOf(indexToFind);
 
         System.out.println("returnValue = " + returnValue);
 
     }
 
-    private static int indexOf(int[] source, int indexToFind) {
-        Arrays.sort(source);
-        int index = Arrays.binarySearch(source, indexToFind);
+    private static int indexOf(int indexToFind) {
+        Arrays.sort(numbers);
+        int index = Arrays.binarySearch(numbers, indexToFind);
 
         if(index >= 0) {
             System.out.println("Index Found!");
